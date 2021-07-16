efc5f7eae6495ec7495aba301a1af382f31254da
dreamheart <leondreamheart@outlook.com>
2021-07-16 12:18:27 -0400

    refactored badges, APACHE batch error fixed

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-// Required modules
+// Constants variables needed for the application
 const fs = require("fs");
 const inquirer = require("inquirer");
-const util = require("util");
-const path = require("path");
+const utility = require("util");
 
-// Get user inputs using inquirer
-const getUserInput = () => {
+// function to write to file
+const writeToFile = utility.promisify(fs.writeFile)
+
+// Main logic, will get the data if sucessful and generate a readme file, otherwise will log error
+getUserInput()
+    .then(function (dataBack) {
+        const outputFile = generateFille(dataBack);
+        return writeToFile("./utility/generateREADME.md", outputFile);
+    })
+
+    .then(function () {
+        console.log("Sucess");
+    })
+
+    .catch(function (err) {
+        console.log(err)
+    })
+
+    // Prompts for the user inputs
+function getUserInput() {
     return inquirer.prompt([
         {
             name: "title",
@@ -18,9 +35,19 @@ const getUserInput = () => {
             message: "Provide a short description for your project"
         },
         {
+            name: "userName",
+            type: "input",
+            message: "What is your github user Name?"
+        },
+        {
+            name: "email",
+            type: "input",
+            message: "What is your email?"
+        },
+        {
             name: "installation",
             type: "input",
-            message: "What command should I run to install this?"
+            message: "What command should I run to install the dependencies?"
         },
         {
             name: "usage",
@@ -30,7 +57,6 @@ const getUserInput = () => {
         {
             name: "license",
             type: "list",
-            // Add more licenses as i do more research
             choices: ["none", "MIT", "GPL 3.0", "APACHE"]
         },
         {
@@ -46,58 +72,52 @@ const getUserInput = () => {
     ])
 }
 
-function createMdFile(response) {
-    // Switch Statement to get the badges from page source
-    let badge;
-    switch (response.license) {
-        case "none":
-            badge = ""
-            break;
+// Will generate the markdown file as well as getting the badge type
+function generateFille(dataBack) {
+    let badge = "";
+    switch (dataBack.license) {
+
+        default:
+        badge = "";
+        break;
 
         case "MIT":
-            badge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]"
+            badge = "![GitHub license](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)"
             break;
 
-        case "GPL 3.0":
-            badge = "![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)"
-            break;
+            case "GPL 3.0":
+                badge = "![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)"
+                break;
 
         case "APACHE":
             badge = "![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)"
             break;
     }
 
-    return `
-    # ${response.title}
-
-    ## Add badge later
-
-    ${response.description}
-
-    ### Table of contents:
-    * [How to Install](#installation)
-    * [How to use it](#usage)
-
-
-
-
-    `
-
+    return `# ${dataBack.title}  ${badge}
+${dataBack.description}
+## Table of Contents:
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+### Installation:
+To install dependencies, run the following:
+\`\`\`${dataBack.installation}\`\`\`
+### Usage:
+${dataBack.usage}
+### License:
+This project is licensed under:
+${dataBack.license}
+### Contributing:
+${dataBack.contribution}
+### Tests:
+To run the test enter the following:
+\`\`\`${dataBack.test}\`\`\`
+### Questions:
+If you have any questions contact me at [GitHub](https://github.com/${dataBack.userName}) or contact me at ${dataBack.email}
+    
+ `
 }
-
-getUserInput()
-
-.then(function(response){
-    let outputFile = createMdFile(response);
-    return writetoFile(path.join(process.cwd(), "readMe"), outputFile);
-})
-
-.then(function () {
-    console.log("Sucess...");
-})
-
-.catch(function (error){
-    console.log(error);
-})
-
-const writetoFile = util.promisify(fs.writeFile);
